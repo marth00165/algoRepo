feef0ddb90efb39dcb227e6c91e1371d6554c786
Rohit <rohitpratti@gmail.com>
2019-12-11 16:21:17 -0800

    100% space

---
+++ productSubtractSum.js
@@ -0,0 +1,20 @@
+let nums = 234
+
+
+let subtractProductAndSum = (number) => {
+
+    let numbers = number.toString(10).split("").map(function (t) { return parseInt(t) }) 
+    const getSum = (total, num) => {
+            return total + num;   
+        }
+    const getProduct = (total, num) => {
+        return total * num;
+    }
+
+    let sum = numbers.reduce(getSum)
+    let product = numbers.reduce(getProduct)
+    let answer = (product - sum)
+    return answer
+};
+
+subtractProductAndSum(nums)
