b5eedac992e5222a8f77b4c3329412bf470a8a7b
Rohit <rohitpratti@gmail.com>
2020-12-26 14:11:34 -0700

    added more optimal space soultion

--- twosum.py
+++ twosum.py
@@ -11,15 +11,43 @@ def twoSum(nums, target):
             hashMap[x] = True  # Else add to map and continue
     return []
 
+# Time Complexity is O(N) because we are only iterating through list once
+
+# Space Complexity is O(N) because we are at worst keeping track of every element in list
+
+
+def twoNumberSum(array, target):
+    array.sort()  # O(NLogN)
+
+    right = len(array) - 1  # Right Pointer
+    left = 0  # Left Pointer
+
+    while left < right:  # While Pointers don't overlap
+        total = array[left] + array[right]  # Temp Sum
+        if total == target:  # If temp sum is target return values
+            return [array[left], array[right]]
+        elif total < target:  # If less than move left pointer
+            left += 1
+        elif total > target:  # If Greater than move right pointer
+            right -= 1
+    return []
+
+# Time Complexity is O(NLogN) assuming quick sort/ merge sort
+
+# Space Complexity is O(1) since we aren't initiating a new hashmap
+
 
 testCase = {"array": [3, 5, -4, 8, 11, 1, -1, 6], "target": 10}
 
 
 def run_tests():
+    answer = [[11, -1], [-1, 11]]
     ans = twoSum(testCase['array'], testCase['target'])
 
-    assert ans == [11, -1] or ans == [-1,
-                                      11], f'Should be [11, -1], returned {ans}'
+    ans2 = twoNumberSum(testCase['array'], testCase['target'])
+
+    assert ans in answer, f'Should be [11, -1], returned {ans}'
+    assert ans2 in answer, f'Should be [11, -1], returned {ans}'
 
     print("All Tests Passed :)")
 
